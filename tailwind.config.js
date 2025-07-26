eb973b4814dd47d413f22762e8a29c676d44e0c0
adityaputraf21 <adityaputraferdiansyah21@gmail.com>
2025-07-26 09:44:59 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Pastikan path ini mencakup semua file komponen Anda
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
